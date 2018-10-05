ce259d88e381526248a1b6e849b440d239000570
DepTrai <deptrailoitaiai@ft.io>
2018-10-05 18:30:04 +0700

    readd util

---
+++ src/util/StringUtil.js
@@ -0,0 +1,23 @@
+export default {
+  htmlToClearText: (html) => {
+    if (html && html.length) {
+      html = html.replace(/&nbsp;/g, ""); // replace space entity
+      html = html.replace(/\s+/g, " "); // replace more than one space by only one.
+      html = html.replace(/<\/?[^>]+(>|$)/g, ""); // replace HTML tags
+      return html;
+    }
+    return "";
+  },
+
+  toUpperCaseFirstChar: (text) => {
+    return text.toLowerCase().toLowerCase().replace(/\b[a-z]/g, function (letter) {
+      return letter.toUpperCase();
+    });
+  },
+
+  removeSlash: (str) => {
+    if (!str) return '';
+    str = str.trim()
+    return str.replace(/\/$/, "");
+  }
+}
